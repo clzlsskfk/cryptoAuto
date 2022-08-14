4658dd4f45e0b2a1c7211c5f9c5dc7e84f327690
clzlsskfk <clzlscjsrnr@gmail.com>
2022-08-14 23:02:53 +0900

    인공지능, 슬랙메세지 추가작업중

--- backtestMA.py
+++ backtestMA.py
@@ -3,11 +3,16 @@ import numpy as np
 import bestk
 
 # OHLCV(open, high, low, close, volume)로 당일 시가, 고가, 저가, 종가, 거래량을 나타내고, count는 몇일간의 테스트인지 작성
-df = pyupbit.get_ohlcv("KRW-ETH", interval="60minute", count=7)
+df = pyupbit.get_ohlcv("KRW-ETH", interval="60minute", count=100)
 
 # 일주일간 최고 수익률의 k값 계산
 print("Best k = %.1f" % max(bestk.listRorK, key=bestk.listRorK.get))
 k = max(bestk.listRorK, key=bestk.listRorK.get)
+df['k'] = k
+
+# 15일 이동 평균선 조회
+dfMa = pyupbit.get_ohlcv("KRW-ETH", interval="day", count=15)
+df['ma15'] = dfMa['close'].rolling(window=15).mean().iloc[-1]
 
 # 변동폭 * k 계산, (고가 - 저가) * k값
 df['range'] = (df['high'] - df['low']) * k
@@ -18,7 +23,7 @@ df['target'] = df['open'] + df['range'].shift(1)
 df['fee_Buy'] = df['target'] * 0.0025
 df['fee_Cell'] = df['close'] * 0.0025
 # ror(수익률), np.where(조건문, 참일때 값, 거짓일때 값)
-df['ror'] = np.where(df['high'] > df['target'] + df['fee_Buy'],
+df['ror'] = np.where((df['high'] > df['target'] + df['fee_Buy']) & (df['ma15'] < df['high']),
                      (df['close'] - df['fee_Cell']) / df['target'],
                      1)
 
--- bestk.py
+++ bestk.py
@@ -2,19 +2,28 @@ import pyupbit
 import numpy as np
 
 
+
 def get_ror(k=0.5):
-    df = pyupbit.get_ohlcv("KRW-BTC", count= 30)
+    df = pyupbit.get_ohlcv("KRW-BTC", interval="60minute", count= 7)
     df['range'] = (df['high'] - df['low']) * k
     df['target'] = df['open'] + df['range'].shift(1)
 
-    df['ror'] = np.where(df['high'] > df['target'],
-                         df['close'] / df['target'],
-                         1)
+    df['fee_Buy'] = df['target'] * 0.0025
+    df['fee_Cell'] = df['close'] * 0.0025
+    df['ror'] = np.where(df['high'] > df['target'] + df['fee_Buy'],
+                     (df['close'] - df['fee_Cell']) / df['target'],
+                     1)
 
     ror = df['ror'].cumprod()[-2]
+
     return ror
 
+listRorK = {}
 
 for k in np.arange(0.1, 1.0, 0.1):
     ror = get_ror(k)
     print("%.1f %f" % (k, ror))
+    listRorK[float('%.1f'%k)] = ror
+
+# print("--------------------------------")
+print("수익률 이라죠 %.1f %f" % (max(listRorK, key=listRorK.get), max(listRorK.values())))
